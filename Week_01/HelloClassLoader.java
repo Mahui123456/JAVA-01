31d71d60f370413e7a0c653c3d901269a5a2d377
mahui <mahui>
2021-01-11 14:12:01 +0800

    作业提交

---
+++ Week_01/HelloClassLoader.java
@@ -0,0 +1,76 @@
+package com.example.demo.controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Base64;
+
+/**
+ * @Author: mahui
+ * @Description:
+ * @Date: Create in 1:11 PM 1/7/21
+ */
+public class HelloClassLoader extends ClassLoader{
+    public static void main(String[] args) {
+        try {
+            Object instance = new HelloClassLoader().findClass("Hello").newInstance();
+            instance.getClass().getMethod("hello").invoke(instance);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException{
+        String helloBase64 = "NQFFQf///8v/4/X/+f/x9v/w/+/3/+71/+3/7Pj/6/j/6v7/+cOWkZaLwf7//NfWqf7/+7yQm5r+//CzlpGasYqSnZqNq56dk5r+//qXmpOTkP7/9ayQio2cmrmWk5r+//W3mpOTkNGVnome8//4//f4/+nz/+j/5/7/7Leak5OQ09+ck56MjLOQnpuajd74/+bz/+X/5P7/+reak5OQ/v/vlZ6JntCTnpGY0LCdlZqci/7/75WeiZ7Qk56RmNCshoyLmpL+//yQiov+/+qzlZ6JntCWkNCvjZaRi6yLjZqeksT+/+yVnome0JaQ0K+NlpGLrIuNmp6S/v/4j42WkYuTkf7/6tezlZ6JntCTnpGY0KyLjZaRmMTWqf/e//r/+f///////f/+//j/9//+//b////i//7//v////rVSP/+Tv////7/9f////n//v////7//v/0//f//v/2////2v/9//7////2Tf/97fxJ//tO/////v/1////9f/9////+//3//r//v/z/////f/y";
+        byte[] bytes = decode(helloBase64);
+        byte[] bytesNew = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            int num = bytes[i];
+            bytes[i]=(byte) (255-num);
+            bytesNew[i] = bytes[i];
+        }
+
+        return defineClass(name,bytesNew,0,bytesNew.length);
+    }
+
+    public byte[] decode(String base64){
+        return Base64.getDecoder().decode(base64);
+    }
+
+    public static String readFileContent(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        StringBuffer sbf = new StringBuffer();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempStr;
+            while ((tempStr = reader.readLine()) != null) {
+                sbf.append(tempStr);
+            }
+            reader.close();
+            return sbf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return sbf.toString();
+    }
+}
